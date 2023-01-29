eec9ea5de9f63a5513140bf40926b8f6ff99747c
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-01-29 22:15:14 +0530

    Modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Layout/Header";
 import React from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Cart/>
       <main>
       <Meals/>
       </main>
