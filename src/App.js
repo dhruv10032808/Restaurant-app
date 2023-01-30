386f6783c3d393bde72fa56f1ef47a20d4dc85e0
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-01-30 22:55:42 +0530

    adding cart items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import React,{useState} from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown,setCartIsShown]=useState(false);
@@ -13,13 +14,13 @@ function App() {
     setCartIsShown(false);
   }
   return (
-    <React.Fragment>
+    <CartProvider>
       <Header onShowCart={showCartHandler}/>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <main>
       <Meals/>
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,25 @@
+import React, {useRef} from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
+
 const MealItemForm=(props)=>{
-   return (<form className={classes.form}>
-    <Input label="Amount" input={{
+    const amountInputRef= useRef();
+   const submitHandler=(event)=>{
+    event.preventDefault();
+    const enteredAmount=amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+}
+   return (<form className={classes.form} onSubmit={submitHandler}>
+    <Input label="Amount" ref={amountInputRef} input={{
         id:"amount_"+props.id,
         type:"number",
         min:"1",
